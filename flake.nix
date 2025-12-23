9dcb2edd62441d914438332462611eca36e512aa
shaman.sir <shaman.sir@gmail.com>
2025-12-23 23:34:54 +0100

    try to configure `nix develop`, p.2

--- flake.nix
+++ flake.nix
@@ -1,56 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs_24_11.url = "github:nixos/nixpkgs/24.11";
     flake-utils.url = "github:numtide/flake-utils";
+    mkSpagoDerivation.url = "github:jeslie0/mkSpagoDerivation";
     ps-overlay.url = "github:thomashoneyman/purescript-overlay";
-    mkSpagoDerivation = {
-      url = "github:jeslie0/mkSpagoDerivation";
-      inputs = {
-        registry.url = "github:purescript/registry/fe3f499706755bb8a501bf989ed0f592295bb4e3";
-        registry-index.url = "github:purescript/registry-index/a349ca528812c89915ccc98cfbd97c9731aa5d0b";
-      };
-    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs_24_11, flake-utils, ps-overlay, mkSpagoDerivation }:
+  outputs = { self, nixpkgs, flake-utils, mkSpagoDerivation, ps-overlay }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ mkSpagoDerivation.overlays.default
-                       ps-overlay.overlays.default
-                     ];
+                       ps-overlay.overlays.default ];
         };
-
-        myPackage =
+      in
+        {
+          packages.default =
             pkgs.mkSpagoDerivation {
               spagoYaml = ./spago.yaml;
               spagoLock = ./spago.lock;
               src = ./.;
+              nativeBuildInputs = [ pkgs.purs-unstable pkgs.spago-unstable pkgs.esbuild ];
               version = "0.1.0";
-              nativeBuildInputs = [ pkgs.esbuild pkgs.purs-backend-es pkgs.purs-bin.purs-0_15_15 pkgs.spago-unstable ]; # nixpkgs_24_11.nodePackages.parcel
-              # changed `pkgs.purs-unstable` to `pkgs.purs-bin.purs-0_15_15`
-              buildPhase = "spago build";
-              installPhase = "mkdir $out; cp -r ./web $out";
+              buildPhase = "spago bundle";
+              installPhase = "mkdir $out; cp index.js $out";
               buildNodeModulesArgs = {
-                  npmRoot = ./.;
+                npmRoot = ./.;
                 nodejs = pkgs.nodejs;
               };
             };
-
-        myApp = {
-            type = "app";
-            program = "sh ./serve.sh";
-        };
-
-      in
-        {
-          packages.default = myPackage;
-
-          apps.default = myApp;
         }
-
     );
 }
