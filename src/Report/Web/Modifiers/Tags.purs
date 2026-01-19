fbe2b1a5d022019bf1410a8bf6a02af4715a767c
shaman.sir <shaman.sir@gmail.com>
2026-01-19 05:47:45 +0100

    remove `user-select` for item's tags

--- src/Report/Web/Modifiers/Tags.purs
+++ src/Report/Web/Modifiers/Tags.purs
@@ -14,14 +14,14 @@ import Report.Web.Helpers (H)
 
 subjTagWrap :: forall subj_tag w i. S.IsTag subj_tag => subj_tag -> H w i
 subjTagWrap tag =
-    HH.span [ HP.style $ "display: inline-block; cursor: pointer; padding: 3px 0;" ] [ subjTagBadge tag ]
+    HH.span [ HP.style $ "display: inline-block; cursor: pointer; padding: 3px 0; user-select: none;" ] [ subjTagBadge tag ]
 
 
 subjTagBadge :: forall subj_tag w i. S.IsTag subj_tag => subj_tag -> H w i
 subjTagBadge tag =
     let tagStyle = S.tagColors tag
     in HH.span
-        [ HP.style $ "font-size: 0.7em; position: relative; top: -1px; margin: 0 0 0 7px; padding: 1px 3px; border-radius: 4px; border: 1px solid " <> tagStyle.border <> "; opacity: 0.8; background-color: " <> tagStyle.background <> "; color: " <> tagStyle.text <> ";" ]
+        [ HP.style $ "font-size: 0.7em; position: relative; top: -1px; margin: 0 0 0 7px; padding: 1px 3px; border-radius: 4px; border: 1px solid " <> tagStyle.border <> "; opacity: 0.8; background-color: " <> tagStyle.background <> "; color: " <> tagStyle.text <> "; user-select: none;" ]
         [ HH.text $ S.tagContent tag ]
 
 
@@ -29,7 +29,7 @@ itemTagBadge :: forall item_tag w i. S.IsTag item_tag => (MouseEvent -> i) -> it
 itemTagBadge onClick tag =
     let tagStyle = S.tagColors tag
     in HH.span
-        [ HP.style $ "font-size: 0.7em; position: relative; top: -1px; margin: 0 0 0 7px; padding: 1px 3px; border-radius: 4px; border: 1px solid " <> tagStyle.border <> "; opacity: 0.8; background-color: " <> tagStyle.background <> "; color: " <> tagStyle.text <> ";"
+        [ HP.style $ "font-size: 0.7em; position: relative; top: -1px; margin: 0 0 0 7px; padding: 1px 3px; border-radius: 4px; border: 1px solid " <> tagStyle.border <> "; opacity: 0.8; background-color: " <> tagStyle.background <> "; color: " <> tagStyle.text <> "; user-select: none;"
         , HE.onClick onClick
         ]
         [ HH.text $ S.tagContent tag ]
