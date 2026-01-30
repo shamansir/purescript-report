404db8cf02737cf46f34bb0bd78ba3516459f0b7
shaman.sir <shaman.sir@gmail.com>
2026-01-30 23:14:17 +0100

    and  helper methods

--- src/Report/Builder.purs
+++ src/Report/Builder.purs
@@ -5,10 +5,16 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
+import Data.Tuple (fst, snd) as Tuple
 import Data.Tuple.Nested ((/\), type (/\))
-import Data.Array (catMaybes) as Array
+import Data.Array ((:))
+import Data.Array (concat, catMaybes, sort, sortWith, sortBy, groupAll, groupAllBy) as Array
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty (head, toArray) as NEA
+import Data.Array.Extra (groupExt, groupExtBy) as Array
 import Data.String (joinWith) as String
-import Data.Bifunctor (class Bifunctor)
+import Data.Bifunctor (class Bifunctor, rmap, lmap)
+import Data.Newtype (class Newtype, wrap, unwrap)
 
 import Report.Chain (Chain)
 import Report.Chain as Chain
@@ -21,6 +27,23 @@ newtype Builder s g i = Builder (Array (Subject s g i))
 data Subject s g i = Subject s (Array (Group g i))
 data Group g i = Group (Chain g) (Array (Item i))
 newtype Item i = Item i
+
+
+derive instance Functor Item
+derive instance Functor (Group g)
+derive instance Functor (Subject s g)
+derive instance Functor (Builder subj group)
+
+derive instance Bifunctor Group
+derive instance Bifunctor (Subject s)
+derive instance Bifunctor (Builder subj)
+
+derive instance Newtype (Builder s g i) _
+derive instance Newtype (Item i) _
+
+
+derive instance Eq a => Eq (Item a)
+derive instance Ord a => Ord (Item a)
 
 
 data TreeNode subj group item  -- both as a source when converting from `Tree` or a target when converting to `Tree`
@@ -46,15 +69,25 @@ derive instance Bifunctor (TreeNodeC subj)
 
 
 toBuilder :: forall subj group item. Array (subj /\ Array (group /\ Array item)) -> Builder subj group item
-toBuilder subjects =
+toBuilder = map (map $ map $ lmap Chain.singleton) >>> toBuilderC
+
+
+toBuilderC :: forall subj group item. Array (subj /\ Array (Chain group /\ Array item)) -> Builder subj group item
+toBuilderC subjects =
     Builder $ toSubjectR <$> subjects
     where
-        toSubjectR :: subj /\ Array (group /\ Array item) -> Subject subj group item
+        toSubjectR :: subj /\ Array (Chain group /\ Array item) -> Subject subj group item
         toSubjectR (subj /\ groupsArr) =
             Subject subj $ toGroupR <$> groupsArr
-        toGroupR :: group /\ Array item -> Group group item
+        toGroupR :: Chain group /\ Array item -> Group group item
         toGroupR (group /\ itemsArr) =
-            Group (Chain.singleton group) (Item <$> itemsArr)
+            Group group (Item <$> itemsArr)
+
+
+unfold :: forall subj group item. Builder subj group item -> Array (subj /\ Array (Chain group /\ Array item))
+unfold = unwrap >>> map \(Subject subj groups) -> subj /\ (unfoldGroup <$> groups)
+    where
+        unfoldGroup (Group groupC items) = groupC /\ (unwrap <$> items)
 
 
 toTree :: forall subj group item. Builder subj group item -> Tree (TreeNode subj group item)
@@ -141,4 +174,132 @@ nodeCToString withPrefix = case _ of
     CNGroup groupC -> if withPrefix then "G: " <> (String.joinWith " ... " $ show <$> Chain.toArray groupC) else (String.joinWith " ... " $ show <$> Chain.toArray groupC)
     CNItem item    -> if withPrefix then "I: " <> show item  else show item
 
+{- Subjects -}
 
+mapSubjects :: forall subjA subjB group item. (subjA -> subjB) -> Builder subjA group item -> Builder subjB group item
+mapSubjects mapSubj = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn :: Subject subjA group item -> Subject subjB group item
+        mapFn (Subject s groups) = Subject (mapSubj s) groups
+
+
+sortSubjects :: forall subj group item. Ord subj => Builder subj group item -> Builder subj group item
+sortSubjects = unwrap >>> Array.sortWith sortWithFn >>> wrap
+    where
+        sortWithFn (Subject s _) = s
+
+
+sortSubjectsWith :: forall subj group item a. Ord a => (subj -> a) -> Builder subj group item -> Builder subj group item
+sortSubjectsWith toA = unwrap >>> Array.sortWith sortWithFn >>> wrap
+    where
+        sortWithFn (Subject s _) = toA s
+
+
+sortSubjectsBy :: forall subj group item. (subj -> subj -> Ordering) -> Builder subj group item -> Builder subj group item
+sortSubjectsBy = sortSubjectsBy' identity
+
+
+sortSubjectsBy' :: forall subj group item a. (subj -> a) -> (a -> a -> Ordering) -> Builder subj group item -> Builder subj group item
+sortSubjectsBy' toA ordFn =unwrap >>> Array.sortBy sortByFn >>> wrap
+    where
+        sortByFn (Subject sA _) (Subject sB _) = ordFn (toA sA) (toA sB)
+
+
+allSubjects :: forall subj group item. Builder subj group item -> Array subj
+allSubjects = unwrap >>> map extractSubj
+    where
+        extractSubj (Subject s _) = s
+
+
+{- Groups -}
+
+
+mapGroups :: forall subj groupA groupB item. (groupA -> groupB) -> Builder subj groupA item -> Builder subj groupB item
+mapGroups = lmap
+
+
+sortGroups :: forall subj group item. Ord group => Builder subj group item -> Builder subj group item
+sortGroups = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups) = Subject s $ Array.sortWith sortWithFn groups
+        sortWithFn (Group gchain _) = Chain.toArray gchain
+
+
+sortGroupsWith :: forall subj group item a. Ord a => (group -> a) -> Builder subj group item -> Builder subj group item
+sortGroupsWith toA = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups) = Subject s $ Array.sortWith sortWithFn groups
+        sortWithFn (Group gchain _) = toA <$> Chain.toArray gchain
+
+
+sortGroupsBy :: forall subj group item. (Chain group -> Chain group -> Ordering) -> Builder subj group item -> Builder subj group item
+sortGroupsBy = sortGroupsBy' identity
+
+
+sortGroupsBy' :: forall subj group item a. (group -> a) -> (Chain a -> Chain a -> Ordering) -> Builder subj group item -> Builder subj group item
+sortGroupsBy' toA ordFn = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups) = Subject s $ Array.sortBy sortByFn groups
+        sortByFn (Group gchainA _) (Group gchainB _) = ordFn (toA <$> gchainA) (toA <$> gchainB)
+
+
+regroup :: forall subj groupA groupB item. Ord groupB => (item -> Chain groupB) -> Builder subj groupA item -> Builder subj groupB item
+regroup = regroupBy compare
+
+
+regroupBy :: forall subj groupA groupB item. (Chain groupB -> Chain groupB -> Ordering) -> (item -> Chain groupB) -> Builder subj groupA item -> Builder subj groupB item
+regroupBy ordFn itemToGroup = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups) = Subject s $ backToGroups $ Array.groupAllBy groupByFn $ allItemsOf groups
+        allItemsOf = map (\(Group _ items) -> addGroup <$> items) >>> Array.concat
+        addGroup (Item i) = itemToGroup i /\ Item i
+        groupByFn (gchainA /\ _) (gchainB /\ _) = ordFn gchainA gchainB
+        backToGroups :: Array (NonEmptyArray (Chain groupB /\ Item item)) -> Array (Group groupB item)
+        backToGroups = map (\nea -> (Tuple.fst $ NEA.head nea) /\ (Tuple.snd <$> NEA.toArray nea)) >>> map (\(groupC /\ items) -> Group groupC items)
+
+
+allGroups :: forall subj group item. Builder subj group item -> Array group
+allGroups = unwrap >>> map extractGroups >>> map Array.concat >>> Array.concat
+    where
+        extractGroups (Subject _ groups) = extractGroupC <$> groups
+        extractGroupC (Group groupC _) = Chain.toArray groupC
+
+
+{- Items -}
+
+
+mapItems :: forall subj group itemA itemB. (itemA -> itemB) -> Builder subj group itemA -> Builder subj group itemB
+mapItems = rmap
+
+
+sortItems :: forall subj group item. Ord item => Builder subj group item -> Builder subj group item
+sortItems = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups)    = Subject s $ sortGroup <$> groups
+        sortGroup (Group gc items) = Group gc $ Array.sort items
+
+
+sortItemsWith :: forall subj group item a. Ord a => (item -> a) -> Builder subj group item -> Builder subj group item
+sortItemsWith toA = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups)    = Subject s $ sortGroup <$> groups
+        sortGroup (Group gc items) = Group gc $ Array.sortWith (unwrap >>> toA) items
+
+
+sortItemsBy :: forall subj group item. (item -> item -> Ordering) -> Builder subj group item -> Builder subj group item
+sortItemsBy = sortItemsBy' identity
+
+
+sortItemsBy' :: forall subj group item a. (item -> a) -> (a -> a -> Ordering) -> Builder subj group item -> Builder subj group item
+sortItemsBy' toA ordFn = unwrap >>> map mapFn >>> wrap
+    where
+        mapFn (Subject s groups) = Subject s $ sortGroup <$> groups
+        sortGroup (Group gc items) = Group gc $ Array.sortBy sortByFn items
+        sortByFn (Item iA) (Item iB) = ordFn (toA iA) (toA iB)
+
+
+allItems :: forall subj group item. Builder subj group item -> Array item
+allItems = unwrap >>> map extractGroups >>> map Array.concat >>> Array.concat
+    where
+        extractGroups (Subject _ groups) = extractGroupC <$> groups
+        extractGroupC (Group _ items) = unwrap <$> items
--- src/Report/Chain.purs
+++ src/Report/Chain.purs
@@ -6,9 +6,12 @@ import Foreign (F, ForeignError(..), fail)
 
 -- import Control.Applicative (class Pure)
 import Data.Array ((:))
-import Data.Array (uncons) as Array
-import Data.Maybe (Maybe(..), maybe)
+import Data.Array (uncons, snoc) as Array
+import Data.List (List(..))
+import Data.FunctorWithIndex (class FunctorWithIndex)
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
 import Data.String (joinWith, split, Pattern(..)) as String
+import Data.Tuple.Nested (type (/\), (/\))
 
 import Yoga.JSON (class WriteForeign, class ReadForeign, readImpl, writeImpl)
 
@@ -21,8 +24,20 @@ data Chain a
 
 
 derive instance Functor Chain
+instance FunctorWithIndex Int Chain where
+    mapWithIndex :: forall a b. (Int -> a -> b) -> Chain a -> Chain b
+    mapWithIndex mapFn = foldF 0
+        where
+            foldF idx = case _ of
+                End a -> End $ mapFn idx a
+                More a rest -> More (mapFn idx a) $ foldF (idx + 1) rest
 
 
+instance Eq a  => Eq  (Chain a) where eq ca cb      = toArray ca == toArray cb
+instance Ord a => Ord (Chain a) where compare ca cb = compare (toArray ca) (toArray cb)
+
+
+--| Get last link
 last :: forall a. Chain a -> a
 last =
     case _ of
@@ -30,6 +45,7 @@ last =
         More _ rest -> last rest
 
 
+--| Get next link, no matter if it's an end or chain continues further
 next :: forall a. Chain a -> a
 next =
     case _ of
@@ -37,26 +53,57 @@ next =
         More a _ -> a
 
 
+--| Get all elements but the first
 tail :: forall a. Chain a -> Maybe (Chain a)
 tail =
     case _ of
         End _ -> Nothing
         More _ rest -> Just rest
 
+--| Get all items before the last and the last one separately.
+break :: forall a. Chain a -> Array a /\ a
+break = foldF []
+    where
+        foldF prev = case _ of
+            End a -> prev /\ a
+            More a rest -> foldF (Array.snoc prev a) rest
+
+
+--| Call one function for when there are more links further and another function when it's an end of the chain
+chain :: forall a b. (a -> Array a -> b) -> (a -> b) -> Chain a -> b
+chain onMore onEnd = case _ of
+    End a -> onEnd a
+    More a rest -> chain (const $ const $ onMore a $ toArray rest) onEnd rest
+    -- More a rest -> chain (\a' rest' -> onMore a $ a' : rest') onEnd rest
+
+
+--| Get all the transformations of chain: `More 1 $ More 2 $ End 3 == [ More 1 $ More 2 $ End 3, More 1 $ End 2, End 1 ]`
+-- subchains :: forall a. Chain a -> Array (Chain a)
+
+--| Get all the transformations of chain: `More 1 $ More 2 $ End 3 == [ [ 1, 2, 3 ], [ 1, 2 ], [ 1 ] ]`
+-- unwraps :: forall a. Chain a -> Array (Array a)
+-- unwraps = foldF []
+--     where
+--         foldF prev = case _ of
+--             End a -> ?wh
+--             More a rest -> ?wh
+
 
 singleton :: forall a. a -> Chain a
 singleton = End
 
 
+--| Create a chain with a single link.
 end :: forall a. a -> Chain a
 end = singleton
 
 
+--| Create a chain with first link and continuation a.k.a. cons
 more :: forall a. a -> Chain a -> Chain a
 more = More
 
 
-toArray :: forall a. Chain a -> Array a
+toArray :: Chain ~> Array
 toArray mbw =
     case mbw of
         End a -> [a]
@@ -79,6 +126,21 @@ toString mbw = String.joinWith "<>" (toArray mbw)
 
 fromString :: String -> Maybe (Chain String)
 fromString str = fromArray (String.split (String.Pattern "<>") str)
+
+
+fromList :: forall a. List a -> Maybe (Chain a)
+fromList = case _ of
+    Nil -> Nothing
+    Cons a Nil -> Just $ End a
+    Cons a rest -> Just $ case fromList rest of
+        Just restChain -> More a restChain
+        Nothing -> End a
+
+
+toList :: Chain ~> List
+toList = case _ of
+    End a -> Cons a Nil
+    More a rest -> Cons a $ toList rest
 
 
 instance WriteForeign (Chain String) where
